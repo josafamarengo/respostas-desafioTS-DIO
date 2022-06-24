e70f2314da2d8d00cf933efe0db83fbbb01b2447
josafamarengo <josafabmarengo@gmail.com>
2022-06-24 15:09:26 -0300

    soluções dos desafios de typescript da DIO - desafio 4 incompleto

---
+++ desafio2.ts
@@ -0,0 +1,35 @@
+enum Profissao {
+    Atriz,
+    Padeiro
+}
+
+interface Pessoa {
+    nome: string,
+    idade: number,
+    profissao: Profissao
+}
+
+const pessoa1: Pessoa =  {
+    nome: "maria",
+    idade: 29,
+    profissao: Profissao.Atriz
+};
+
+const pessoa2: Pessoa = {
+    nome: "roberto",
+    idade: 19,
+    profissao: Profissao.Padeiro
+}
+
+
+const pessoa3: Pessoa = {
+    nome: "laura",
+    idade: 32,
+    profissao: Profissao.Atriz
+};
+
+const pessoa4: Pessoa = {
+    nome: "carlos",
+    idade: 19,
+    profissao: Profissao.Padeiro
+}
---
+++ desafio3.ts
@@ -0,0 +1,40 @@
+let botaoAtualizar = document.getElementById('atualizar-saldo');
+let botaoLimpar = document.getElementById('limpar-saldo')!;
+let soma = document.getElementById('soma')! as HTMLInputElement;
+let campoSaldo = document.getElementById('campo-saldo');
+
+let total = 0
+
+limparSaldo()
+
+function somarAoSaldo(soma: number) {
+    if(campoSaldo) {
+        total += soma
+        campoSaldo.innerHTML = total.toString();
+        limparCampoSoma();
+    }
+}
+
+function limparCampoSoma() {
+    soma.value = "";
+}
+
+function limparSaldo() {
+    if(campoSaldo) {
+        total = 0;
+        campoSaldo.innerHTML = total.toString();
+    }
+}
+
+if(botaoAtualizar){
+
+    botaoAtualizar.addEventListener('click', function () {
+        somarAoSaldo(Number(soma.value));
+    });
+
+}
+
+
+botaoLimpar.addEventListener('click', function () {
+    limparSaldo();
+});
